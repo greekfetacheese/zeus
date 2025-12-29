66850a2462fc92f76ac4d5ad460a8b4ac63234f5
Michalis Molfetas <mixalismolfetas@gmail.com>
2025-12-29 11:14:36 +0200

    chore: update egui to v0.33.3 and local crates to v0.3.0

--- crates/zeus-widgets/src/lib.rs
+++ crates/zeus-widgets/src/lib.rs
@@ -1,11 +1,14 @@
-pub mod button;
-pub mod combo_box;
-pub mod label;
-pub mod multi_label;
-pub mod secure_text_edit;
+mod button;
+mod combo_box;
+mod label;
+mod multi_label;
+mod secure_text_edit;
 
 pub use button::Button;
 pub use combo_box::ComboBox;
 pub use label::Label;
 pub use multi_label::MultiLabel;
 pub use secure_text_edit::SecureTextEdit;
+
+#[cfg(feature = "secure-types")]
+pub use secure_types::{SecureString, Zeroize};
